e634ae755ae1b6ce6c89b7ff5ef086c7bf4dcfea
Tony Spiro <tspiro@tonyspiro.com>
2015-10-30 12:07:51 -0500

    rm stuff

--- actions.js
+++ actions.js
@@ -1,7 +1,7 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-export function getBucket(AppStore){
+export function getBucket(Store){
   
   fetch('https://api.cosmicjs.com/v1/wedding-site')
     .then(response => {
@@ -11,7 +11,7 @@ export function getBucket(AppStore){
       return response.json()
     })
     .then(response => {
-      AppStore.bucket = response.bucket
-      AppStore.emitChange();
+      Store.bucket = response.bucket
+      Store.emitChange();
     })
 }

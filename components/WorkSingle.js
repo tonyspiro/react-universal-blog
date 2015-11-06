e0116e795adb7e459f2b1cb4dd4f7b08382da1e5
Tony Spiro <tspiro@tonyspiro.com>
2015-11-05 21:18:01 -0600

    :rocket:

--- components/WorkSingle.js
+++ components/WorkSingle.js
@@ -23,7 +23,7 @@ class WorkSingle extends Component {
 
     return (
       <div>
-        <Link to="/work" className="btn btn-default" style={ style }>&lt&lt Back to Work List</Link>
+        <Link to="/work" className="btn btn-default" style={ style }>&lt;&lt; Back to Work List</Link>
         <h2>{ work_item.title }</h2>
         <div dangerouslySetInnerHTML={ {__html: work_item.content } }></div>
       </div>

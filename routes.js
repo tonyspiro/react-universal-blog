8f373d4af93d3f09039ce37ae0880d9001306fad
Tony Spiro <tspiro@tonyspiro.com>
2015-12-01 00:32:46 -0600

    routes clean up

--- routes.js
+++ routes.js
@@ -10,18 +10,18 @@ import App from './components/App'
 
 // Routes
 import Blog from './components/routes/Blog'
-import Work from './components/routes/Work'
 import Default from './components/routes/Default'
+import Work from './components/routes/Work'
 import NoMatch from './components/routes/NoMatch'
 
 export default (
   <Route path="/" data={AppStore.data} component={App}>
+    <IndexRoute component={Blog}/>
     <Route path="about" component={Default}/>
     <Route path="contact" component={Default}/>
     <Route path="work" component={Work}/>
     <Route path="/work/:slug" component={Work}/>
     <Route path="/blog/:slug" component={Blog}/>
-    <IndexRoute component={Blog}/>
     <Route path="*" component={NoMatch}/>
   </Route>
 )

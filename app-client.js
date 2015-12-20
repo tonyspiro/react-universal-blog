92de204bfc1cb1c12f395aec9ac00c015dae2e0f
Tony Spiro <tspiro@tonyspiro.com>
2015-12-20 11:11:40 -0600

    Update app-client.js

--- app-client.js
+++ app-client.js
@@ -1,7 +1,7 @@
 // app-client.js
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Link } from 'react-router'
+import { Router } from 'react-router'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 const history = createBrowserHistory()
 

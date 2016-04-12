a5a3060b4999fffc68b565d6c1eaf127d173922e
Tony Spiro <tspiro@tonyspiro.com>
2016-04-12 18:40:00 -0500

    Update NoMatch.js

--- components/Pages/NoMatch.js
+++ components/Pages/NoMatch.js
@@ -39,7 +39,8 @@ export default class NoMatch extends Component {
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
               <div className="text-center">
-                Whoa!  Looks like you stumbled down a worm hole!
+                Whoa!  Looks like you stumbled down a worm hole!<br />
+                If this is a new page that you've added in Cosmic JS, make sure you add it to your <code>routes.js</code> file!
                 <br/>
                 <br/>
                 <Link to="/">Take me home</Link>

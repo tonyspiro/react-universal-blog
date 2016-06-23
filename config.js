22a9dd6303982b387de5cb32c838cc09ea70a4ea
Tony Spiro <tspiro@tonyspiro.com>
2016-06-23 12:16:54 -0500

    read write keys

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ export default {
   },
   bucket: {
     slug: process.env.COSMIC_BUCKET || 'react-universal-blog',
-    media_url: 'https://cosmicjs.com/uploads'
-  }
+    media_url: 'https://cosmicjs.com/uploads',
+    read_key: process.env.COSMIC_READ_KEY || '',
+    write_key: process.env.COSMIC_WRITE_KEY || ''
+  },
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET)
+      'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET),
+      'process.env.COSMIC_READ_KEY': JSON.stringify(process.env.COSMIC_READ_KEY),
+      'process.env.COSMIC_WRITE_KEY': JSON.stringify(process.env.COSMIC_WRITE_KEY)
     })
  ]
 };
